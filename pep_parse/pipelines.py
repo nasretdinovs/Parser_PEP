834e5a12487b108c65b47491affef0f7413f08c7
Виталий Насретдинов <nasretdinovs@yandex.ru>
2022-09-13 21:44:46 +0300

    Fix pipelines

--- pep_parse/pipelines.py
+++ pep_parse/pipelines.py
@@ -11,7 +11,6 @@ class PepParsePipeline:
         self.status_summary = defaultdict()
 
     def open_spider(self, spider):
-        keys = ['Статус', 'Количество']
         now = dt.datetime.now()
         current_date = now.strftime("%Y-%m-%d_%H-%M-%S")
         filename = 'status_summary'
@@ -19,20 +18,14 @@ class PepParsePipeline:
         downloads_dir.mkdir(exist_ok=True)
         infile = open('{}/{}_{}.csv'.format(
             downloads_dir, filename, current_date), 'w')
-
-        self.dict_writer = csv.DictWriter(infile, keys)
-        self.dict_writer.writeheader()
+        self.writer = csv.writer(infile)
+        self.writer.writerow(['Статус', 'Количество'])
 
     def process_item(self, item, spider):
-
         status = item['status']
-        if status in self.status_summary:
-            self.status_summary[status] += 1
-        else:
-            self.status_summary[status] = 1
+        self.status_summary[status] = self.status_summary.get(status, 0) + 1
         return item
 
     def close_spider(self, spider):
         self.status_summary['Total'] = sum(self.status_summary.values())
-        for key, value in self.status_summary.items():
-            self.dict_writer.writerow({'Статус': key, 'Количество': value})
+        self.writer.writerows(list(self.status_summary.items()))
